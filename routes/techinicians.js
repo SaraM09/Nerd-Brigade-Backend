d9b1045e8f7cc1db867b0bb55481958b5ec5f426
dttrue <danieltorres@pursuit.org>
2024-04-16 21:20:28 -0400

    Completed techinicians controllers and routes

---
+++ routes/techinicians.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { getAllTechnicians, createTechnician, updateTechnician, deleteTechnician, getTechnicianById } from '../controllers/techinicians.js';
+
+const router = express.Router();
+
+router.get('/', getAllTechnicians);
+
+router.post('/', createTechnician);
+
+router.get('/:id', getTechnicianById);
+
+router.put('/:id', updateTechnician);
+
+router.delete('/:id', deleteTechnician);
+
+export default router
