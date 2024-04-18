32ba1eff7b04f3eebeba51959baa434c2c31d457
dttrue <danieltorres@pursuit.org>
2024-04-18 10:58:36 -0400

    Completed stripe setup with new payment table

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ import cartItemsRouter from "./routes/cartItems.js";
 import bookingsRouter from "./routes/bookings.js";
 import techiniciansRouter from "./routes/technicians.js";
 import accessoriesRouter from "./routes/accessories.js";
-
+import Stripe from "stripe";
+import bodyParser from "body-parser";
 
 
 
@@ -14,11 +15,18 @@ import accessoriesRouter from "./routes/accessories.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+
+
 
 // routes
 app.use('/users', usersRouter);
